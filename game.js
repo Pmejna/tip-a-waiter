ac658008844caba6a4a26b7cedc429b9a63eb70c
Przemyslaw Mejna <pmejna@gmail.com>
2020-06-18 19:41:56 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,4 @@
+const waiterImg = document.querySelector('.image');
+const content = document.querySelector('.content');
+const contentCount = document.querySelector('.content__count');
+const contentAfter = document.querySelector('.content__after');
