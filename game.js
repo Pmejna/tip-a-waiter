5df28585150b52c223f2540575cfbb00e698d4f2
Przemyslaw Mejna <pmejna@gmail.com>
2020-06-18 20:14:16 +0100

    create html structure for count content

--- game.js
+++ game.js
@@ -2,3 +2,4 @@ const waiterImg = document.querySelector('.image');
 const content = document.querySelector('.content');
 const contentCount = document.querySelector('.content__count');
 const contentAfter = document.querySelector('.content__after');
+const amount = document.querySelector('.content__amount span');
