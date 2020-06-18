6fb0143bae4afba4995a46234d52e7222188b3a9
Przemyslaw Mejna <pmejna@gmail.com>
2020-06-18 20:44:33 +0100

    create HTML structure for after action

--- game.js
+++ game.js
@@ -3,3 +3,7 @@ const content = document.querySelector('.content');
 const contentCount = document.querySelector('.content__count');
 const contentAfter = document.querySelector('.content__after');
 const amount = document.querySelector('.content__amount span');
+
+class Game {
+    
+}
