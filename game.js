c05218d084b2c2515a5b9e9e7a0509029877e985
Przemyslaw Mejna <pmejna@gmail.com>
2020-06-19 15:14:21 +0100

    update image, create object Game

--- game.js
+++ game.js
@@ -12,129 +12,143 @@ const nextBtn = document.querySelector('.content__after button')
 
 const scorePts = document.querySelector('.score__points');
 
-let gameStatus = 0;
-let result = 'start';
-let score = 0;
-let randNum = () => Math.floor(Math.random()*1000);
-let cost = 0;
+class Game {
+    constructor(gameStatus = 0, result = 'start', cost = 0) {
+        this.gameStatus = gameStatus;
+        this.result = result;
+        this.score = 0;
+        this.cost = cost;
+        this.numberGiven = "";
+    }
 
-const setImageTxt = () => {
-    switch (result) {
-        case 'start':
-            waiterImg.src = "/img/Group 2.png";
-            break;
-        case 'low':
-            waiterImg.src = "/img/Group 5.png";
-            title.style.color = "red";
-            title.textContent = "NOT ENOUGH!";
-            subtitle.textContent = "He might be a homeless soon";
-            break;
-        case 'good':
-            waiterImg.src = "/img/Group 3.png";
-            title.style.color = "#32E925";
-            title.textContent = "WELL DONE!";
-            subtitle.textContent = "You gave the right tip!";
-            break;
-        case 'high':
-            waiterImg.src = "/img/Group 4.png";
-            title.style.color = "red";
-            title.textContent = "TOO MUCH";
-            subtitle.textContent = "Other waiters hate him now!";
-            break;
-    }
-}
-const checkStatus = () => {
-    if (!gameStatus) {
-        contentAfter.style.display = "none";
-        contentCount.style.display = "block";
-        putTheValue();
-    }
-    else {
-        contentCount.style.display = "none";
-        contentAfter.style.display = "flex";
-    }
-}
-const putTheValue = () => {
-    let mealCost = randNum();
-    cost = mealCost;
-    amount.textContent = "$"+mealCost;
-}
+    randNum = () => Math.floor(Math.random()*1000);
 
-const checkTheInput = () => {
+    setImageTxt = () => {
+        switch (this.result) {
+            case 'start':
+                waiterImg.src = "/img/Group 2.png";
+                break;
+            case 'low':
+                waiterImg.src = "/img/Group 5.png";
+                title.style.color = "red";
+                title.textContent = "NOT ENOUGH!";
+                subtitle.textContent = "He might be a homeless soon";
+                break;
+            case 'good':
+                waiterImg.src = "/img/Group 3.png";
+                title.style.color = "#32E925";
+                title.textContent = "WELL DONE!";
+                subtitle.textContent = "You gave the right tip!";
+                break;
+            case 'high':
+                waiterImg.src = "/img/Group 4.png";
+                title.style.color = "red";
+                title.textContent = "TOO MUCH";
+                subtitle.textContent = "Other waiters hate him now!";
+                break;
+        }
+    }
+
+    checkStatus = () => {
+        if (!this.gameStatus) {
+            contentAfter.style.display = "none";
+            contentCount.style.display = "block";
+            this.putTheValue();
+        }
+        else {
+            contentCount.style.display = "none";
+            contentAfter.style.display = "flex";
+        }
+    }
+
+    putTheValue = () => {
+        let mealCost = this.randNum();
+        this.cost = mealCost;
+        amount.textContent = "$"+mealCost;
+    }
+
+    checkTheInput = () => {
     
-}
-
-const scoreRender = () => {
-    const scoreColor = () => {
-        if (score < 0) {
-            scorePts.style.color = "red";
-        }
-        else if (score > 0) {
-            scorePts.style.color = "#32E925";
-        }
-        else {
-            scorePts.style.color = "white";
-        }
     }
-    scoreColor();
-    scorePts.textContent = score;
-}
 
-const initialise = () => {
-    setImageTxt();
-    scoreRender();
-    checkStatus();
-}
-
-const setCorrectTip  = () => {
-    let tip = cost*0.15;
-    let minTip = tip - (tip*0.1);
-    let maxTip = tip + (tip*0.1);
-    const isCorrect = (minTip, maxTip) => {
-        if (numberGiven > minTip && numberGiven < maxTip) {
-            result = 'good';
-            score++;
-            scoreRender();
-            gameStatus = 1;
-            checkStatus();
-            setImageTxt();
+    scoreRender = () => {
+        const scoreColor = () => {
+            if (this.score < 0) {
+                scorePts.style.color = "red";
+            }
+            else if (this.score > 0) {
+                scorePts.style.color = "#32E925";
+            }
+            else {
+                scorePts.style.color = "white";
+            }
         }
-        else if (numberGiven < minTip) {
-            result = 'low';
-            score--;
-            scoreRender();
-            gameStatus = 1;
-            checkStatus();
-            setImageTxt();
-        }
-        else {
-            result = 'high';
-            score--;
-            scoreRender();
-            gameStatus = 1;
-            checkStatus();
-            setImageTxt();
-        }
-    }  
-    isCorrect(minTip, maxTip);
-}
-
-const changeToNum = () => {
-    numberGiven = Number(input.value);
-    if (input.value !== '' && !isNaN(numberGiven)) {
-        setCorrectTip();
+        scoreColor();
+        scorePts.textContent = this.score;
     }
-    else {}
+
+    setCorrectTip  = () => {
+        let tip = this.cost*0.15;
+        let minTip = tip - (tip*0.1);
+        let maxTip = tip + (tip*0.1);
+        const isCorrect = (minTip, maxTip) => {
+            if (this.numberGiven > minTip && this.numberGiven < maxTip) {
+                this.result = 'good';
+                this.score++;
+                this.scoreRender();
+                this.gameStatus = 1;
+                this.checkStatus();
+                this.setImageTxt();
+            }
+            else if (this.numberGiven < minTip) {
+                this.result = 'low';
+                this.score--;
+                this.scoreRender();
+                this.gameStatus = 1;
+                this.checkStatus();
+                this.setImageTxt();
+            }
+            else {
+                this.result = 'high';
+                this.score--;
+                this.scoreRender();
+                this.gameStatus = 1;
+                this.checkStatus();
+                this.setImageTxt();
+            }
+        }  
+        isCorrect(minTip, maxTip);
+    }
+
+    changeToNum = () => {
+        this.numberGiven = Number(input.value);
+        if (input.value !== '' && !isNaN(this.numberGiven)) {
+            this.setCorrectTip();
+        }
+        else {}
+    }
+
+    initialise = () => {
+        this.setImageTxt();
+        this.scoreRender();
+        this.checkStatus();
+    }
+
+    setListerners = () => {
+        inputBtn.addEventListener('click', function () {
+            game.changeToNum();
+        })
+        nextBtn.addEventListener('click', function () {
+            game.gameStatus = 0;
+            game.result = "start";
+            game.initialise();
+        })
+    }
+
+
 }
 
+const game = new Game();
+game.setListerners();
+game.initialise();
 
-inputBtn.addEventListener('click', function () {
-    changeToNum();
-})
-nextBtn.addEventListener('click', function () {
-    gameStatus = 0;
-    result = "start";
-    initialise();
-})
-
-initialise();
